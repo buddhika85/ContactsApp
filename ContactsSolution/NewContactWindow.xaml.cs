f1035dd391afda47ed6249398d8dc7a4e4eec211
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-08-11 15:28:49 +0530

    new contact window added

---
+++ ContactsSolution/NewContactWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+
+namespace ContactsSolution
+{
+    /// <summary>
+    /// Interaction logic for NewContactWindow.xaml
+    /// </summary>
+    public partial class NewContactWindow : Window
+    {
+        public NewContactWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
