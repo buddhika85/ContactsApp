3b4da9c3dafd8cea5d15749298464a52ba31dd6e
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-08-16 11:08:15 +0530

    added contacts table poco

---
+++ ContactsSolution/Classes/Contact.cs
@@ -0,0 +1,22 @@
+﻿using SQLite;
+
+namespace ContactsSolution.Classes
+{
+    [Table("Contacts")]
+
+    public class Contact
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(200)]
+        public string Name { get; set; } = null!;
+
+        [Unique]
+        [MaxLength(100)]
+        public string Email { get; set; } = null!;
+
+        [MaxLength(20)]
+        public string Phone { get; set; } = null!;
+    }
+}
