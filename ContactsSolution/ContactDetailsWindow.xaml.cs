0486379ed891ed48d4c436fa8c60ead584c49ae0
Buddhika Dinesh Gunasekera <buddhika2006system@gmail.com>
2023-08-25 13:44:06 +0530

    update contact

--- ContactsSolution/ContactDetailsWindow.xaml.cs
+++ ContactsSolution/ContactDetailsWindow.xaml.cs
@@ -8,15 +8,26 @@ namespace ContactsSolution
     /// </summary>
     public partial class ContactDetailsWindow : Window
     {
-        public readonly Contact _contact;
+        private readonly Contact _contact;
         public ContactDetailsWindow(Contact contact)
         {
-            _contact = contact;
             InitializeComponent();
+            _contact = contact;
+            PopulateForm();
+        }
+
+        private void PopulateForm()
+        {
+            NameTxtBox.Text = _contact.Name;
+            PhoneTxtBox.Text = _contact.Phone;
+            EmailTxtBox.Text = _contact.Email;
         }
 
         private void UpdateBtn_OnClick(object sender, RoutedEventArgs e)
         {
+            _contact.Name = NameTxtBox.Text;
+            _contact.Phone = PhoneTxtBox.Text;
+            _contact.Email = EmailTxtBox.Text;
             DataAccess.UpdateContact(_contact);
             Close();
         }
